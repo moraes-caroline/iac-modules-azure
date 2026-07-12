da5f4b11fc3fb8ed8043a7d563d5d294ee5d3f85
Caroline <9904587@prestadorserv.com.br>
2026-07-12 15:50:16 -0300

    Update private endpoint

--- infra/modules/private-endpoint/main.tf
+++ infra/modules/private-endpoint/main.tf
@@ -1,4 +1,5 @@
 resource "azurerm_private_endpoint" "this" {
+
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -8,17 +9,15 @@ resource "azurerm_private_endpoint" "this" {
   private_service_connection {
     name                           = "${var.name}-psc"
     private_connection_resource_id = var.target_resource_id
+    is_manual_connection           = false
+    subresource_names              = var.subresource_names
+  }
 
-    is_manual_connection = false
+  private_dns_zone_group {
+    name = "default"
 
-    subresource_names = var.subresource_names
+    private_dns_zone_ids = var.private_dns_zone_ids
   }
 
   tags = var.tags
-}
-
-private_dns_zone_group {
-  name = "default"
-
-  private_dns_zone_ids = var.private_dns_zone_ids
 }
