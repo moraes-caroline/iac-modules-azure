3e64da9168505ea0e040155e14e95c175145041a
Caroline <9904587@prestadorserv.com.br>
2026-07-12 16:58:48 -0300

    Update service plan

--- infra/modules/service-plan/main.tf
+++ infra/modules/service-plan/main.tf
@@ -1,36 +1,8 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-resource "azurerm_service_plan" "app_plan" {
+resource "azurerm_service_plan" "this" {
   name                = var.service_plan_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
 
   os_type  = "Linux"
   sku_name = "B1"
-}
-
-############################
-# FRONTEND REACT
-############################
-
-resource "azurerm_linux_web_app" "frontend" {
-  name                = var.frontend_app_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  service_plan_id     = azurerm_service_plan.app_plan.id
-
-  site_config {
-    application_stack {
-      node_version = "20-lts"
-    }
-  }
-
-  app_settings = {
-    API_URL = var.api_url
-  }
-
-  https_only = true
 }
